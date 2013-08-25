3d3176e3c9249d5167636c89c38839dec9cdca28
Shakeel Mohamed <Shakeel_Mohamed@live.com>
2013-08-25 00:08:21 -0700

    Added a super barebones page using jade & bootstrap

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require("express");
+var jade = require('jade');
+
 var app = express();
-app.use(express.logger());
+app.configure(function(){
+	app.use(express.logger());
+	app.use(express.static(__dirname + '/public'));
+});
+
+var html = jade.renderFile('index.jade');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
