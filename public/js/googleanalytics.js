44c7e88e275002216a47346209ca9eba3e8ac039
Shakeel Mohamed <smohamed@splunk.com>
2014-05-05 22:50:40 -0700

    Initial branding for r/Anger; change GA; remove favicon

--- public/js/googleanalytics.js
+++ public/js/googleanalytics.js
@@ -3,7 +3,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
 
-ga("create", "UA-49978518-1", {
+ga("create", "UA-49978518-2", {
   "cookieDomain": "none"
 });
 ga("send", "pageview");
