d0d092ea01ddf4acf89c97a40aefd5a536c0442b
Shakeel Mohamed <smohamed@splunk.com>
2014-05-05 23:05:42 -0700

    Force a push

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "siteName": "r/Anger Journal",
     "siteAuthor": "r/Anger",
-    "siteDescription": "A simple mood journal, for readers of r/Anger",
+    "siteDescription": "A simple mood journal, for readers of r/Anger.",
     "feedbackForm": "https://angerjournal.wufoo.com/forms/m140grm41h4u59j",
     "port": 5000
 };
